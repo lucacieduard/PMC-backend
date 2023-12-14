6117acdfd2955fe6e8b78c375ee7a633115238c7
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-12-14 11:50:39 +0200

    configurare .env

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import { app } from "./app.js";
+import dotenv from "dotenv";
+
 const port = 3000;
+
+dotenv.config({ path: "./.env" });
+// console.log(process.env.TEST);
 
 app.listen(port, () => {
   console.log("App running on port " + port);
