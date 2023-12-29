ebb564d73ff71e848b7f626e99b16034b04c7d48
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-12-29 16:25:07 +0200

    upload & resize files

--- controllers/competitionController.js
+++ controllers/competitionController.js
@@ -1,6 +1,43 @@
 import slugify from "slugify";
 import Competition from "../models/competitionModel.js";
 import catchAsync from "../utils/catchAsync.js";
+import multer from "multer";
+import sharp from "sharp";
+
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "public/img/banners");
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = file.mimetype.split("/")[1];
+//     cb(null, `banner-${req.params.id}.${ext}`);
+//   },
+// });
+
+const multerStorage = multer.memoryStorage();
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) cb(null, true);
+  else cb(new Error("Incarcati o imagine!"), false);
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+export const uploadBanner = upload.single("banner");
+
+export const resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+  req.file.filename = `banner-${req.params.id}.webp`;
+  sharp(req.file.buffer)
+    .resize(1500, 800, { fit: "cover" })
+    .toFormat("webp")
+    .webp()
+    .toFile(`public/img/banners/${req.file.filename}`);
+
+  next();
+};
 
 // GET ALL COMPETITIONS
 export const getAllCompetitions = catchAsync(async (req, res) => {
@@ -76,12 +113,11 @@ export const updateProba = catchAsync(async (req, res, next) => {
 
 // UPDATE ONE COMPETITION
 export const updateCompetition = catchAsync(async (req, res, next) => {
-  console.log(req.body);
-  if(req.body.nume) {
+  if (req.body.nume) {
     const slug = slugify(req.body.nume, { lower: true });
     req.body.slug = slug;
   }
-
+  if (req.file) req.body.banner = req.file.filename;
   const newCompetition = await Competition.findByIdAndUpdate(
     req.params.id,
     req.body,
@@ -90,7 +126,6 @@ export const updateCompetition = catchAsync(async (req, res, next) => {
       new: true,
     }
   );
-
 
   res.status(200).json({
     status: "success",
--- routes/competitionRoutes.js
+++ routes/competitionRoutes.js
@@ -6,6 +6,8 @@ import {
   deleteCompetition,
   updateProba,
   updateCompetition,
+  uploadBanner,
+  resizeUserPhoto,
 } from "../controllers/competitionController.js";
 
 export const router = express.Router();
@@ -14,5 +16,7 @@ router.get("/", getAllCompetitions);
 router.post("/", addCompetition);
 
 router.get("/:id", getCompetition);
-router.delete("/:id", deleteCompetition).patch("/:id", updateCompetition);
+router
+  .delete("/:id", deleteCompetition)
+  .patch("/:id", uploadBanner, resizeUserPhoto, updateCompetition);
 router.patch("/:id/:categoryId/:probaId", updateProba);
