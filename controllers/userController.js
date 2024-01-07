7172d7e254992938e71a3c5c8ef1314c2ec1bfd5
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2024-01-07 13:43:28 +0200

    user delete, update controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,3 +9,23 @@ export const getAllUsers = catchAsync(async (req, res, next) => {
     data: users,
   });
 });
+
+export const deleteUser = catchAsync(async (req, res, next) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
+export const updateUser = catchAsync(async (req, res, next) => {
+  const newUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  console.log(newUser);
+  res.status(200).json({
+    status: "success",
+    data: { newUser },
+  });
+});
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,7 +7,11 @@ import {
   restrictTo,
   signup,
 } from "../controllers/authController.js";
-import { getAllUsers } from "../controllers/userController.js";
+import {
+  deleteUser,
+  getAllUsers,
+  updateUser,
+} from "../controllers/userController.js";
 
 export const router = express.Router();
 
@@ -16,4 +20,7 @@ router.post("/autentificare", login);
 router.get("/persistLogin", persist);
 router.get("/deconectare", logout);
 
-router.get("/",protect,restrictTo("admin"), getAllUsers)
+router.get("/", protect, restrictTo("admin"), getAllUsers);
+router
+  .delete("/:id", protect, restrictTo("admin"), deleteUser)
+  .patch("/:id", protect, restrictTo("admin"), updateUser);
